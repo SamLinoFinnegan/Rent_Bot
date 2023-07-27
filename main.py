169853a87b3cf5d8e3424b06b2004af73f5dcac7
Samuel L Finnegan <73761381+SamLinoFinnegan@users.noreply.github.com>
2023-07-27 22:25:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from dotenv import load_dotenv
 import os
 from datetime import datetime
 import re
 import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from dotenv import load_dotenv
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,9 +15,6 @@ load_dotenv()
 # Access the environment variables
 username = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
-
-
-
 
 # setting the options
 options = webdriver.ChromeOptions()
@@ -33,50 +32,74 @@ options.add_argument('--allow-running-insecure-content')
 options.add_argument(f'user-agent={USER_AGENT}')
 
 
-class Rent_bot:
+class RentBot:
+    """
+    A class representing a rental property bot that automates searching and sending inquiries.
+
+    Attributes:
+        user_data (dict): A dictionary containing user-specific data such as 'full_name', 'your_email',
+                          'your_phone', 'message', and 'params'.
+        url (str): The URL of the website from which the bot will scrape rental property listings.
+        max_price (int): The maximum price limit for the rental properties the bot will consider.
+
+    Methods:
+        __init__(self, user_data, url, max_price):
+            Initializes the Rent_Bot object with the provided user_data, url, and max_price.
+            It sets up the Selenium WebDriver for scraping.
+
+        log_in(self, username, password):
+            Logs into the website with the given username and password.
+            This method is used to access specific features that require authentication.
+
+        run_bot(self):
+            Runs the bot to search for rental properties based on the user's preferences and sends inquiries.
+            It navigates through the website, filters listings based on parameters in 'user_data', and sends inquiries
+            for suitable properties. The bot tracks viewed links to avoid duplicate inquiries.
+    """
     def __init__(self,user_data, url, max_price):
         self.user_data = user_data
         self.url = url
         self.max_price = max_price
-        self.driver = webdriver.Chrome(options=options)
-
-    
-    
-    def log_in(self,username, password):
-
+        self.driver = webdriver.Chrome()
+   
+    def log_in(self,user_name, user_password):
+        """
+        log_in(self, username, password):
+        Logs into the website with the given username and password.
+        This method is used to access specific features that require authentication.
+        """
         elem = self.driver.find_element(By.ID, 'username')
-        elem.send_keys(username)
-
+        elem.send_keys(user_name)
         password_elem = self.driver.find_element(By.ID, 'password')
-        password_elem.send_keys(password)
-
+        password_elem.send_keys(user_password)
         time.sleep(5)
-
         login_parent = self.driver.find_element(By.CLASS_NAME, 'login__cta-section')
         login_button = login_parent.find_element(By.TAG_NAME, 'input')
         login_button.click()
 
-        
-
-
     def run_bot(self):
+        """
+        run_bot(self):
+            Runs the bot to search for rental properties based on the user's preferences and sends inquiries.
+            It navigates through the website, filters listings based on parameters in 'user_data', and sends inquiries
+            for suitable properties. The bot tracks viewed links to avoid duplicate inquiries.
+        """
 
         # fixed variables
-        new_hits = 0
+        _new_hits = 0
 
+        # start by getting the query URL
         self.driver.implicitly_wait(5)
-
         self.driver.get(self.url)
 
         try:
             pop_up = self.driver.find_element(By.XPATH, '//*[@id="js-cookie-modal-level-one"]/div/main/div/button[2]')
             pop_up.click()
             
-        except:
+        except NoSuchElementException:
             print("There wasnt any pop up")
 
         # check if there are any new places that match my query
-
         trigger = self.driver.find_element(By.XPATH, '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/h1')
 
         if not trigger.text.startswith("0"): # if not 0 places
@@ -90,12 +113,12 @@ class Rent_bot:
             # check my record to see if i already sent a message to the ad
             file_name = self.user_data["full_name"]
             file_name = file_name.replace(" ", "")
-            
+
             if os.path.isfile(file_name+".txt"):
-                with open(file_name+".txt", "r") as file:
-                    viewed_links.read()
+                with open(file_name+".txt", "r", encoding="utf-8") as file:
+                    viewed_links = file.read()
             else:
-                with open(file_name+".txt", "w+") as file:
+                with open(file_name+".txt", "w+", encoding="utf-8") as file:
                     viewed_links = file.read()
 
             copy_a_href = [link.get_attribute("href") for link in a]
@@ -103,8 +126,6 @@ class Rent_bot:
             first_iteration = True
 
             for url in copy_a_href:
-               
-
                 if url in viewed_links:
                     print("The URL was already hit")
                     continue
@@ -116,7 +137,7 @@ class Rent_bot:
 
                 # Check a few parameters
                 month_or_week = self.driver.find_element(By.XPATH, '//*[@id="__next"]/main/div[3]/div[1]/div[1]/div/div[2]/div[1]/h2')
-                parameters = self.driver.find_elements(By.CLASS_NAME, 'styles__ListLabel-sc-15fxapi-10')                
+                parameters = self.driver.find_elements(By.CLASS_NAME, 'styles__ListLabel-sc-15fxapi-10')
 
                 today = datetime.now().date()
 
@@ -132,22 +153,23 @@ class Rent_bot:
                             number_in_date = re.search(r'\d+', date_string)
 
                             if len(number_in_date.group()) <= 5:
-                                date_string = date_string[:date_string.index(number_in_date.group())] +"0"+ date_string[date_string.index(number_in_date.group()):]
-                               
+                                date_string = date_string[
+                                    :date_string.index(
+                                    number_in_date.group())] +"0"+ date_string[
+                                    date_string.index(number_in_date.group()
+                                                      ):
+                                    ]
+                       
                             target_date = datetime.strptime(date_string, "%b%d%Y").date()
                             days_difference = (target_date - today).days
 
                     if (
-                        self.user_data["params"][split_str[0]] == split_str[1]
-                        or split_str[0] in self.user_data["params"]["ignore"]
-                        or self.user_data["params"]["Available From"] >= days_difference
+                        self.user_data["params"][split_str[0]] != split_str[1]
+                        or split_str[0] not in self.user_data["params"]["ignore"]
+                        or self.user_data["params"]["Available From"] <= days_difference
                     ):
-                        time.sleep(10)
-                        pass
-                    else:
                         continue
-
-
+                    
                 if month_or_week.text.endswith("week"):
                     match = re.search(r'\d+', month_or_week.text)
                     price = int(match.group())
@@ -155,22 +177,24 @@ class Rent_bot:
                         continue
                     else:
                         print(f"its {price * 4} per month")
-                time.sleep(10)
+                time.sleep(5)
 
-                button = self.driver.find_element(By.XPATH, '/html/body/div[2]/main/div[3]/div[2]/div/div[1]/div[2]/div[2]/button')
-                button.click()
+                try:
+                    login_button = self.driver.find_element(By.XPATH, '/html/body/div[2]/main/div[3]/div[2]/div/div[1]/div[2]/div[2]/button')
+                    login_button.click()
 
+                except NoSuchElementException:
+                    continue
+                    
                 #log in
                 if first_iteration:
                     first_iteration = False
 
-                    time.sleep(10)
-                    self.log_in(username=username,password=password)
+                    time.sleep(5)
+                    self.log_in(user_name=username,user_password=password)
                     
-
-                
-                # fill in the form  
-                time.sleep(5)          
+                # fill in the form
+                time.sleep(5)
                 name = self.driver.find_element(By.ID, 'keyword1')
                 name.send_keys(self.user_data['full_name'])
                 email = self.driver.find_element(By.ID, 'keyword2')
@@ -189,40 +213,37 @@ class Rent_bot:
                 try:
                     submit_button = self.driver.find_element(By.XPATH, '/html/body/div[9]/div/div/div[2]/form/div/div[5]/div/button')
                     submit_button.click()
-                except:
+                except NoSuchElementException:
                     print("It didnt find the send button by xpath")
                 try:
                     parent = self.driver.find_element(By.XPATH, '//*[@id="contact-form-modal"]/div[2]/form/div/div[5]/div')
                     submit_button = parent.find_element(By.TAG_NAME, 'button')
                     submit_button.click()
-                except:
+                except NoSuchElementException:
                     print("It didnt find the send button from parent")
 
                 file_name = self.user_data["full_name"]
                 file_name = file_name.replace(" ", "")
 
-                with open(file_name+".txt", "a") as file:
+                with open(file_name+".txt", "a",encoding="utf-8") as file:
                     file.write("{}\n".format(copy_url))
 
                 time.sleep(3)
                 try:
                     close_button = self.driver.find_element(By.XPATH, '/html/body/div[9]/div/div/div[1]/button')
                     close_button.click()
-                except:
+                except NoSuchElementException:
                     print("It didnt find the close button from full xpath")
-                try:
-                    close_button = self.driver.find_element(By.XPATH, '//*[@id="contact-form-modal"]/div[1]/button')
-                    close_button.click()
-                except:
-                    print("It didnt find the close button from relative xpath")
+               
 
-                new_hits += 1
-                time.sleep(2)
-            print(new_hits)
+                _new_hits += 1
+                time.sleep(3)
+            print(_new_hits)
             self.driver.close()
 
         else:
             print("There are no new places")
-            self.driver.close()   
+            self.driver.close()
 
-        time.sleep(100)
+
+
